600254df25805226614f80d68a4dbfda53978ec5
Avdhesh Kumar Sharma <avdhesh.sharma@allonblock.com>
2019-03-18 16:57:31 +0530

    Simplified the code for TextValidation.jsx and posList.jsx file

--- src/components/common/TextValidation.jsx
+++ src/components/common/TextValidation.jsx
@@ -3,26 +3,52 @@ import Input from 'material-ui/Input/Input';
 import FormControl from 'material-ui/Form/FormControl';
 import FormHelperText from 'material-ui/Form/FormHelperText';
 
-
-export function GenericInput({ ariaDescribedBy,htmlFor,displayName,inputName,defaultValue,disabled,onChange,onPaste,
-    onBlur, errorMessage, error, errorValue, className, touched, touchedValue, errorCheck=true, type='text'}) {
+export function GenericInput
+    ({ 
+        ariaDescribedBy,
+        htmlFor,
+        displayName,
+        inputName,
+        defaultValue,
+        disabled,
+        onChange,
+        onPaste,
+        onBlur, 
+        errorMessage, 
+        error, 
+        errorValue, 
+        className, 
+        touched,
+        dirty, 
+        touchedValue, 
+        errorCheck=true,
+        type="text"
+    }) 
+{
+    console.log('Is touched: ', touched);
     return (
-        <FormControl className="custom-input" style={{width: "100%", fontSize: "14px" }}>
+        <FormControl 
+            className="custom-input" 
+            style={{width: "100%", fontSize: "14px" }}
+        >
             {/* <InputLabel htmlFor={htmlFor}>{displayName}</InputLabel> */}
-            <Input name={inputName} value={defaultValue} type={type}
-            disabled={disabled}
-            placeholder={displayName}
-            onChange={onChange}
-            onPaste = {onPaste}
-            onBlur={onBlur} />
-            {errorCheck && error && errorValue && touched && touchedValue && (
-            <div className={className}><FormHelperText >
-            {errorMessage}
-            </FormHelperText></div> 
+            <Input 
+                name={inputName} 
+                value={defaultValue} 
+                type={type}
+                disabled={disabled}
+                placeholder={displayName}
+                onChange={onChange}
+                onPaste = {onPaste}
+                onBlur={onBlur} 
+            />
+            {errorCheck && touched && dirty && (
+                <div className={className}>
+                    <FormHelperText>
+                        {errorMessage}
+                    </FormHelperText>
+                </div> 
             )}              
         </FormControl>
     )
-    
-   
-    
-  }
+}
