2ccba909853a77e3474f5437e331b6905970e7af
jaipur <yogendra.jain@allonblock.com>
2019-04-09 16:42:47 +0530

    added code for from and to calander

---
+++ src/Global/Components/FromandToCalander.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+/* Lodash Imports */
+import _get from 'lodash/get';
+/* Material import */
+
+/* Redux Imports */
+
+/* Component Imports */
+import DatePicker from 'react-datepicker';
+import AutoComplete from '../../components/Elements/AutoComplete';
+
+
+
+class From extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    dateSelectHandler = (type) => {
+        return (date) => {
+            this.setState({
+                [type]: date
+            });
+            this.props.onDateSelect(type, date);
+        }
+    }
+    storeSelectHandler = (id) => {
+        this.setState({ selectedStore: id });
+        this.props.onStoreSelect(id);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <div>
+                        <label>From: </label>
+                    </div>
+                    <DatePicker
+                        selected={this.state.startDate}
+                        selectsStart
+                        showYearDropdown={true}
+                        startDate={this.state.startDate}
+                        onSelect={this.dateSelectHandler("startDate")}
+                        className="form-control"
+                    />
+                </div>
+                {this.props.doNotShowTo ? null : <div>
+                    <div class="head-title">
+                        <label>To: </label>
+                    </div>
+                    <DatePicker
+                        selected={this.state.endDate}
+                        selectsStart
+                        showYearDropdown={true}
+                        startDate={this.state.startDate}
+                        onSelect={this.dateSelectHandler("endDate")}
+                        className="form-control"
+                    />
+                </div>}
+                <div style={{ width: "25%" }}>
+                    <label>Select Store</label>
+                    <AutoComplete
+                        type="single"
+                        data={this.props.storeList}
+                        name="stores"
+                        value={_get(this.state, 'selectedStore', '')}
+                        changeHandler={this.storeSelectHandler}
+                    />
+                </div>
+            </React.Fragment>
+
+        );
+    }
+}
+
+export default From;
+
+
+
+//Using this component
+
+/*<FromandToCalander
+    doNotShowTo={true} //if you dont want TODate to be shown
+    onDateSelect={this.onDateSelect}
+    storeList={this.state.storeList} //pass store list in this format
+    onStoreSelect={this.onStoreSelect}
+/>*/
+
+
+
+// implement this handler in your component
+//  onDateSelect = (type, value) => {
+//      this.setState({ [type]: value })
+//  }
+// onStoreSelect = (id) => {
+//     this.setState({id})
+//    }
--- src/containers/Dashboard/DashboardContainer.jsx
+++ src/containers/Dashboard/DashboardContainer.jsx
@@ -3,23 +3,66 @@ import React from 'react';
 import _get from 'lodash/get';
 
 /* Material import */
+import Button from "@material-ui/core/Button"
 
 /* Redux Imports */
-
+import { connect } from 'react-redux';
 /* Component Imports */
 import HighVelocityChart from './Components/HighVelocityChart';
 import LowVelocityChart from './Components/LowVelocityChart';
 import PaymentMethodsPie from './Components/PaymentMethodsPie';
 import LowInventoryTable from './Components/LowInventoryTable';
 import OutOfStockTable from './Components/OutOfStockTable';
+import genericPostData from '../../Global/DataFetch/genericPostData';
+import FromandToCalander from '../../Global/Components/FromandToCalander';
 
 class DashboardContainer extends React.Component {
 
     constructor() {
         super();
         this.state = {
-
+            storeList: []
         }
+    }
+    componentDidMount() {
+        // genericPostData({
+        //     url: "/Reports/SalesReport/ByPaymentMethods",
+        //     reqObj: {
+        //         FromTimeStamp: {seconds:323},
+        //         ToTimeStamp: {seconds:323}
+        //     },
+        //     dispatch:this.props.dispatch,
+        //     identifier:"GETPIEDATA",
+        //     successCb:console.log("data is here"),
+        //     errorCb:()=>console.log("err is here")
+        // })
+        genericPostData({
+            url: "/Store/ByRetailerId",
+            reqObj: {
+                id: localStorage.getItem('retailerID')
+            },
+            dispatch: this.props.dispatch,
+            identifier: "getStoreByRetailer_dashboard",
+            successCb: (storeList) => {
+                let storeListArr = storeList.map((store) => {
+                    let displayObj = {
+                        displayText: store.name,
+                        value: store.id
+                    }
+                    return displayObj
+                });
+                this.setState({ storeList: storeListArr });
+
+            },
+            errorCb: () => console.log("err is here"),
+            dontShowMessage: true
+        })
+    }
+    onDateSelect = (type, value) => {
+        this.setState({ [type]: value })
+    }
+    onStoreSelect = (id) => {
+     this.setState({id})
     }
 
     render() {
@@ -28,6 +71,22 @@ class DashboardContainer extends React.Component {
                 <div className='panel-container'>
                     <span className='panel-heading'>Dashboard</span>
                 </div>
+                <div style={{padding:"20px"}} className="flex-row dash-1 justify-space-between">
+                    <FromandToCalander
+                        doNotShowTo ={true}
+                        onDateSelect={this.onDateSelect}
+                        storeList={this.state.storeList}
+                        onStoreSelect={this.onStoreSelect}
+                    />
+                    
+                    <div className="flex-row align-center">
+                        <Button onClick={this.getReports} variant="contained" color="primary">
+                            Submit
+                   </Button>
+                    </div>
+
+                </div>
+
                 <div className='fwidth flex-column'>
 
                     <div className='flex-row dash-1'>
@@ -60,13 +119,13 @@ class DashboardContainer extends React.Component {
                         <div className='dash-21'>
                             <div className='card flex-column fheight'>
                                 <span className='card-title'>Low Velocity Items</span>
-                                <LowVelocityChart/>
+                                <LowVelocityChart />
                             </div>
                         </div>
                         <div className='dash-22'>
                             <div className='card flex-column payment-method-pie fheight'>
                                 <span className='card-title'>Payment Methods</span>
-                                <PaymentMethodsPie/>
+                                <PaymentMethodsPie />
                             </div>
                         </div>
                     </div>
@@ -76,15 +135,15 @@ class DashboardContainer extends React.Component {
                     <div className='flex-row dash-3'>
                         <div className='card fwidth'>
                             <span className='card-title'>Low Inventory</span>
-                            <LowInventoryTable/>
-                        </div>   
+                            <LowInventoryTable />
+                        </div>
                     </div>
-                    
+
                     <div className='flex-row dash-4'>
                         <div className='card fwidth'>
                             <span className='card-title'>Out Of stock</span>
-                            <OutOfStockTable/>
-                        </div>   
+                            <OutOfStockTable />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -92,4 +151,8 @@ class DashboardContainer extends React.Component {
     }
 }
 
-export default DashboardContainer;
+function mapStateToProps() {
+    return {};
+}
+
+export default connect(mapStateToProps)(DashboardContainer);
