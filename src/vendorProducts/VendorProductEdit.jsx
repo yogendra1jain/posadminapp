c78d0adfa811afd60744cb0151159432a6ab094d
Yogendra Jain <yogendra.jain@allonblock.com>
2019-05-25 12:16:20 +0530

    added code for vendor products

--- src/vendorProducts/VendorProductEdit.jsx
+++ src/vendorProducts/VendorProductEdit.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Edit, TextInput, ReferenceInput, NumberInput, BooleanInput } from 'react-admin';
+import { Edit, TextInput, ReferenceInput, NumberInput, BooleanInput, SimpleForm, DateInput, SelectInput } from 'react-admin';
 export const VendorProductEdit = props => (
     < Edit {...props }>
         <SimpleForm>
-            <TextInput source="id" />
-            <ReferenceInput source="vendorId" reference="vendors"><SelectInput optionText="id" /></ReferenceInput>
-            <ReferenceInput source="posProductId" reference="posProducts"><SelectInput optionText="id" /></ReferenceInput>
-            <ReferenceInput source="retailerId" reference="retailers"><SelectInput optionText="id" /></ReferenceInput>
-            <DateInput source="sku" />
+            <ReferenceInput source="vendorId" reference="vendors"><SelectInput optionText="name" /></ReferenceInput>
+            <ReferenceInput source="posProductId" reference="Search/Products"><SelectInput optionText="name" /></ReferenceInput>
+            <TextInput source="sku" />
             <TextInput source="price.currency" />
             <NumberInput source="defaultOrderQty" />
             <NumberInput source="conversionFactor" />
