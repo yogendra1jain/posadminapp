c41873aca2dc53a7e25b088c52f8d8a26b0f50d6
shubham chitransh <shubham.chitransh@allonblock.com>
2019-05-28 16:30:27 +0530

    removed unnecessary commented code

--- src/products/ProductEdit.jsx
+++ src/products/ProductEdit.jsx
@@ -1,38 +1,3 @@
-// import { TextField, BooleanField, ReferenceField, DateField, NumberField, Datagrid, List, } from 'react-admin';
-// import React from 'react';
-// export const ProductList = props => (
-//     <List {...props}>
-//         <Datagrid rowClick="edit">
-//             <TextField source="sku" />
-//             <TextField source="name" />
-//             <TextField source="costPrice.amount" />
-//             <TextField source="salePrice.amount" />
-//             {/* <TextField source="id" />
-//             <TextField source="name" />
-//             <TextField source="description" />
-//             <TextField source="category1" />
-//             <TextField source="category2" />
-//             <TextField source="category3" />
-//             <TextField source="image" />
-//             <BooleanField source="active" />
-//             <TextField source="salePrice.currency" />
-//             <TextField source="sku" />
-//             <TextField source="upcCode" />
-//             <BooleanField source="isTaxable" />
-//             <ReferenceField source="retailerId" reference="retailers"><TextField source="id" /></ReferenceField>
-//             <TextField source="costPrice.currency" />
-//             <BooleanField source="discountable" />
-//             <NumberField source="createdOn.seconds" />
-//             <DateField source="dimensions" />
-//             <TextField source="keywords" />
-//             <DateField source="extendedSku" />
-//             <TextField source="seasonality" />
-//             <NumberField source="updatedOn.seconds" />
-//             <TextField source="additionalUpcCodes" /> */}
-//         </Datagrid>
-//     </List>
-// );
-
 import React from "react";
 import {
   translate,
