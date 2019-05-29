18d0c91feab36eba65cd41b108f547c6a8e82ad1
Yogendra Jain <yogendra.jain@allonblock.com>
2019-05-29 10:01:27 +0530

    removed hardcoding of application bff url

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,11 +1,13 @@
 // in src/authProvider.js
 import { AUTH_LOGIN, AUTH_CHECK, AUTH_LOGOUT, AUTH_ERROR } from 'react-admin';
 import jwtDecode from 'jwt-decode';
+import { APPLICATION_BFF_URL } from './global/UrlConstants';
+
 
 const authProvider = (type, params) => {
     if (type === AUTH_LOGIN) {
         const { username, password } = params;
-        const request = new Request('http://13.126.59.19:20029/api/login/admin-login', {
+        const request = new Request(`${APPLICATION_BFF_URL}login/admin-login`, {
             method: 'POST',
             body: JSON.stringify({ email: username, password }),
             headers: new Headers({ 'Content-Type': 'application/json' }),
