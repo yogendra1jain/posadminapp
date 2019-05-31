27a296c4249de76bab9089cded4b19df13e016d2
Marine Asalia <marine.asalia@allonblock.com>
2019-05-31 19:04:27 +0530

    some vendor view changes

--- src/vendorProducts/VendorProductList.jsx
+++ src/vendorProducts/VendorProductList.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import { TextField, ReferenceField, DateField, NumberField, BooleanField, List, Datagrid } from 'react-admin';
+import React from "react";
+import DineroPrice from "../global/components/DineroPrice";
+import {
+  TextField,
+  ReferenceField,
+  NumberField,
+  BooleanField,
+  List,
+  Datagrid,
+  EditButton
+} from "react-admin";
 const VendorListTitle = ({ record }) => {
-    return (
-        <span>
-            Vendor Products List
-        </span>
-    )
+  return <span>Vendor Products List</span>;
 };
 const VendorProductList = props => (
-    <List {...props} title={<VendorListTitle/>}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
-            <ReferenceField source="vendorId" reference="vendors"><TextField source="name" /></ReferenceField>
-            <ReferenceField source="posProductId" reference="Search/Products"><TextField source="name" /></ReferenceField>
-            <DateField source="sku" />
-            <TextField source="price.currency" />
-            <NumberField source="defaultOrderQty" />
-            <NumberField source="conversionFactor" />
-            <BooleanField source="primary" />
-            <TextField source="upc" />
-            <NumberField source="discountPercent" />
-            <TextField source="unitType" />
-        </Datagrid>
-    </List>
+  <List {...props} title={<VendorListTitle />}>
+    <Datagrid rowClick="edit">
+      {/* <TextField source="id" /> */}
+      <ReferenceField source="vendorId" reference="vendors">
+        <TextField source="name" />
+      </ReferenceField>
+      <ReferenceField source="posProductId" reference="Search/Products" label="POS Product">
+        <TextField source="name" />
+      </ReferenceField>
+      <TextField source="sku" label="Vendor SKU"/>
+      <DineroPrice label="Price" source="price.amount" />
+
+      {/* <NumberField source="defaultOrderQty" /> */}
+      <NumberField source="conversionFactor" label="Pack Size"/>
+      <BooleanField source="primary" label="Primary Supplier"/>
+      <EditButton />
+    </Datagrid>
+  </List>
 );
 
-export default VendorProductList
+export default VendorProductList;
--- src/vendors/VendorEdit.jsx
+++ src/vendors/VendorEdit.jsx
@@ -1,17 +1,31 @@
-
-import { Edit, SimpleForm, TextInput,NumberInput } from 'react-admin';
-import React from 'react';
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  NumberInput,
+  ArrayInput,
+  SimpleFormIterator
+} from "react-admin";
+import React from "react";
 
 const VendorEdit = props => (
-    <Edit {...props}>
-        <SimpleForm>
-            <TextInput source="name" />
-            <TextInput source="email" />
-            <NumberInput label='Country Code' source="phoneNumber.countryCode"/>
-            <NumberInput label='Phone Number' source="phoneNumber.phoneNumber"/>
+  <Edit {...props} undoable={false}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <TextInput source="email" />
 
-        </SimpleForm>   
-    </Edit>
+      <NumberInput label="Country Code" source="phoneNumber.countryCode" />
+      <NumberInput label="Phone Number" source="phoneNumber.phoneNumber" />
+
+      <ArrayInput source="licenses">
+        <SimpleFormIterator>
+          <TextInput source="license" />
+          <TextInput source="type" />
+          <TextInput source="info" />
+        </SimpleFormIterator>
+      </ArrayInput>
+    </SimpleForm>
+  </Edit>
 );
 
 export default VendorEdit;
