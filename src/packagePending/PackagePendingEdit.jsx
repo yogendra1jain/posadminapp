0d42b352ed6718214ec75980c8e6f99730fedb28
Mayuk <mayuk.agarwal@allonblock.com>
2019-06-04 11:03:23 +0530

    Spilt Package UI

--- src/packagePending/PackagePendingEdit.jsx
+++ src/packagePending/PackagePendingEdit.jsx
@@ -1,35 +1,42 @@
 
-import { Edit, SimpleForm, TextInput, BooleanInput, ReferenceInput, AutocompleteInput, SelectInput, TextField, AutoComplete, FormInput, ArrayInput, SimpleFormIterator, DateInput } from 'react-admin';
+import { Edit, TabbedForm, FormTab, TextInput, BooleanInput, ReferenceInput, AutocompleteInput, SelectInput, TextField, AutoComplete, FormInput, ArrayInput, SimpleFormIterator, DateInput } from 'react-admin';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles/withStyles';
 import Quantity from './Quantity';
+import SplitPackageForm from './SplitPackageForm';
 
 const PackagePendingEdit = props => (
     <Edit {...props} >
-        <SimpleForm >
-            <TextField source="id"  />
-            <TextField source="metricProduct" />
-            {/* <ReferenceInput source="uId" reference="us"><SelectInput optionText="id" /></ReferenceInput> */}
-            <ReferenceInput source="posProductId" reference="Search/Products">
-                <AutocompleteInput optionText="name" />
-            </ReferenceInput>
-            {/* <Quantity /> */}
-            <ArrayInput source="Split Package" style={{ width: '100%' }}>
-                <SimpleFormIterator>
-                    <ReferenceInput source="posProductId" reference="Search/Products" label='Product Name'>
-                        <AutocompleteInput optionText="name" />
-                    </ReferenceInput>
-                    <TextInput source="quantity" label='qty' />
-                </SimpleFormIterator>
-            </ArrayInput>
-            <TextField source="manifest" />
-            <TextInput source="facility" />
-            <TextInput source="productCategory" />
-            <TextInput source="quantity" />
-            <TextInput source="vendor" />
-            <TextInput source="lastModified" />
-            <BooleanInput source="metricApproved" />
-        </SimpleForm>
+        <TabbedForm >
+            <FormTab label='Product Details'>
+                <TextField source="id" />
+                <TextField source="metricProduct" />
+                {/* <ReferenceInput source="uId" reference="us"><SelectInput optionText="id" /></ReferenceInput> */}
+                <ReferenceInput source="posProductId" reference="Search/Products">
+                    <AutocompleteInput optionText="name" />
+                </ReferenceInput>
+                {/* <Quantity /> */}
+                <ArrayInput source="Split Package" style={{ width: '100%' }}>
+                    <SimpleFormIterator>
+                        <ReferenceInput source="posProductId" reference="Search/Products" label='Product Name'>
+                            <AutocompleteInput optionText="name" />
+                        </ReferenceInput>
+                        <TextInput source="quantity" label='qty' />
+                    </SimpleFormIterator>
+                </ArrayInput>
+                <TextField source="manifest" />
+                <TextInput source="facility" />
+                <TextInput source="productCategory" />
+                <TextInput source="quantity" />
+                <TextInput source="vendor" />
+                <TextInput source="lastModified" />
+                <BooleanInput source="metricApproved" />
+            </FormTab>
+            <FormTab label="Scans">
+                <SplitPackageForm {...props} />
+            </FormTab>
+
+        </TabbedForm>
     </Edit>
 );
 
