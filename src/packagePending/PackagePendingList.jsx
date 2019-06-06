2ee49417ff692d54bebe1376af30d58d380f9f4a
Yogendra Jain <yogendra.jain@allonblock.com>
2019-06-06 11:03:32 +0530

    added code for package pending

--- src/packagePending/PackagePendingList.jsx
+++ src/packagePending/PackagePendingList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { List, Datagrid, TextField, ReferenceField, BooleanField, DateField, EditButton } from 'react-admin';
-import { FormDataConsumer } from 'ra-core';
+import { List, Datagrid, TextField, ReferenceField, BooleanField, DateField, EditButton, FormDataConsumer } from 'react-admin';
+import InfoOutline from '@material-ui/icons/InfoOutline'
+
 
 const PackagePendingList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
+        <Datagrid >
             <TextField label="Id" source="packageID" />
             {/* <ReferenceField source="uId" reference="us"><TextField source="id" /></ReferenceField> */}
             <TextField label="Manifest Number" source="manifestNumber" />
@@ -19,9 +20,22 @@ const PackagePendingList = props => (
             <TextField label="Shipped Quantity" source="shippedQuantity" />
             <TextField label="UOM" source="shippedUnitOfMeasureName" />
             <FormDataConsumer>
-                {(fd)=>fd.shippedQuantity==""}
+                {({ formData, ...rest }) => {
+                    debugger;
+                    if (formData.shipmentPackageState == "Shipped") {
+                        return <InfoOutline titleAccess="Please Accept Package at Metric Site" color="red"></InfoOutline>
+                    }
+                    else if (formData.shipmentPackageState == "Accepted") {
+                        return <EditButton label="Check In" />
+                    }
+                    else {
+                        return null
+                    }
+                }
+                }
             </FormDataConsumer>
-            <EditButton label="Check In" />
+
+
 
         </Datagrid>
     </List>
