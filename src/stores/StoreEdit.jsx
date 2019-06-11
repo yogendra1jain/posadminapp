01879a04da46b27a0d2b4865139326f75bfb37dc
Marine Asalia <marine.asalia@allonblock.com>
2019-06-11 17:28:55 +0530

    store edits

--- src/stores/StoreEdit.jsx
+++ src/stores/StoreEdit.jsx
@@ -1,28 +1,21 @@
-import { TextInput,
-    Edit,
-    SimpleForm,
-    SelectArrayInput,
-    Loading,
-    Query
-} from 'react-admin';
-import React from 'react';
-import ZipCodeInput from '../global/components/ZipCodeInput';
-import _get from 'lodash/get';
-import _find from 'lodash/find';
-import _isEmpty from 'lodash/isEmpty';
+import { TextInput, Edit, SimpleForm, NumberInput } from "react-admin";
+import React from "react";
+import ZipCodeInput from "../global/components/ZipCodeInput";
+import _get from "lodash/get";
+import _find from "lodash/find";
+import _isEmpty from "lodash/isEmpty";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const StoreEditTitle = ({ record }) => {
-    return (
-        <span>
-             {record ? `${record.name}` : ''}
-        </span>
-    )
+  return <span>{record ? `${record.name}` : ""}</span>;
 };
 
 // let createSource = (paymentMethods, availablePaymentMethods) => {
 //     let source = paymentMethods.map(paymentMethod => {
 //         return _find(availablePaymentMethods,'id',paymentMethod)
-//     }) 
+//     })
 //     return source
 // }
 
@@ -39,20 +32,60 @@ const StoreEditTitle = ({ record }) => {
 // }
 
 const StoreEdit = props => (
-    <Edit title={<StoreEditTitle />} {...props}>
-        <SimpleForm>
-            <TextInput label="Store Name" source="name" />
-            <TextInput label="Address Line 1" source="address.addressLine1" />
-            <TextInput label="Address Line 2" source="address.addressLine2" />
-            <ZipCodeInput source="address.postalCode" />
-            <TextInput label="City" source="address.city" />
-            <TextInput label="State" source="address.state" />
-            <TextInput label="Country" source="address.country" />
-            {/* <TextInput label="StoreKey" source="storeKey" /> */}
-            {/* <TextInput label="License No." source="licenseNo" /> */}
-            {/* <PaymentMethods /> */}
-        </SimpleForm>
-    </Edit>
+  <Edit title={<StoreEditTitle />} {...props}>
+    <SimpleForm>
+      <TextInput label="Store Name" source="name" />
+      <TextInput label="Address Line 1" source="address.addressLine1" />
+      <TextInput label="Address Line 2" source="address.addressLine2" />
+      <ZipCodeInput source="address.postalCode" />
+      <TextInput label="City" source="address.city" />
+      <TextInput label="State" source="address.state" />
+      <TextInput label="Country" source="address.country" />
+      <div style={{ width: 400, margin: "1em" }} />
+
+      <Card>
+        <CardContent>
+          <Typography>MED Purchase Limits</Typography>
+
+          <NumberInput
+            source="medLimit.weightLimit"
+            label="Cannabis Limit (g)"
+          />
+          <NumberInput
+            source="medLimit.concentrateLimit"
+            label="Concentrate Limit (g)"
+          />
+          <NumberInput
+            source="medLimit.plantCountLimit"
+            label="Plant Limit (nos.)"
+          />
+        </CardContent>
+      </Card>
+      <div style={{ width: 400, margin: "1em" }} />
+      <Card>
+        <CardContent>
+          <Typography>REC Purchase Limits</Typography>
+          <NumberInput
+            source="recLimit.weightLimit"
+            label="Cannabis Limit (g)"
+          />
+          <NumberInput
+            source="recLimit.concentrateLimit"
+            label="Concentrate Limit (g)"
+          />
+          <NumberInput
+            source="recLimit.plantCountLimit"
+            label="Plant Limit (nos.)"
+          />
+        </CardContent>
+      </Card>
+      <CardContent />
+
+      {/* <TextInput label="StoreKey" source="storeKey" /> */}
+      {/* <TextInput label="License No." source="licenseNo" /> */}
+      {/* <PaymentMethods /> */}
+    </SimpleForm>
+  </Edit>
 );
 
 export default StoreEdit;
