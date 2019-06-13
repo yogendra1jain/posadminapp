0019512feeb24ce05fa89569a6796524480f115f
Yogendra Jain <yogendra.jain@allonblock.com>
2019-06-13 12:30:40 +0530

    removed bug of synching  disable of metric

--- src/packagePending/PackagePendingList.jsx
+++ src/packagePending/PackagePendingList.jsx
@@ -4,49 +4,49 @@ import InfoOutline from '@material-ui/icons/InfoOutline'
 import moment from 'moment'
 import SyncIcon from '@material-ui/icons/Sync';
 import Button from '@material-ui/core/Button'
+import uuidv1 from 'uuid/v1';
 
 
-const ListActionButton = ({ basePath, data, resource,syncClick,lastSynched,disable }) => {
+const ListActionButton = ({ basePath, data, resource, syncClick, lastSynched, disable }) => {
     return (
-    <div>
-    <Button disabled={disable}  color="primary" onClick={syncClick}>Sync Now</Button>
-    <span>{`Last Synced ${moment(lastSynched * 1).format('MM/DD/YYYY hh:mm a')}`}</span>
-    </div>
+        <div>
+            <Button disabled={disable} color="primary" onClick={syncClick}>Sync Now</Button>
+            <span>{`Last Synced ${moment(lastSynched * 1).format('MM/DD/YYYY hh:mm a')}`}</span>
+        </div>
     )
 }
 
 class PackagePendingList extends React.Component {
-constructor(props){
-    super(props);
-    this.state = {lastSynched:null,disable:false}
-}
-    setTimOutCb = ()=>{
-        console.log("^^^^^^^^^^^^^^^")
-        this.setState({disable:false});
-        this.forceUpdate()
+    constructor(props) {
+        super(props);
+        this.state = { lastSynched: null, disable: false, key: uuidv1() }
+    }
+    setTimOutCb = () => {
+        this.setState({ disable: false, key: uuidv1() });
     }
     syncClick = (props) => {
-        this.setState({disable:true})
-        setInterval(this.setTimOutCb,5*1000)
+        this.setState({ disable: true })
+        setTimeout(this.setTimOutCb, 60 * 1000)
         this.props.dataProvider('GET_ONE', 'Sync/Metrc/IncomingTransfers', { id: localStorage.getItem('retailerId') }).then((data) => {
-            localStorage.setItem('lastSynched',Date.now())
-            this.setState({lastSynched:Date.now(),disable:false});
+            localStorage.setItem('lastSynched', Date.now())
+            this.setState({ lastSynched: Date.now(),key:uuidv1() });
         })
-       
-    } 
-    componentDidMount(){
-        this.setState({lastSynched:localStorage.getItem('incomingPkgSyncTime')})
-    }  
+
+    }
+    componentDidMount() {
+        this.setState({ lastSynched: localStorage.getItem('incomingPkgSyncTime') })
+    }
 
     render() {
         return (
             <div className='flex-column'>
-                    {/* <div className='flex-row justify-center align-center justify-flex-end'>
-                        <span>{`METRC Last Synced ${moment(localStorage.getItem('incomingPkgSyncTime') * 1).format('MM/DD/YYYY hh:mm a')}`}</span>
-                        <SyncIcon onClick={this.syncClick} style={{ fontSize: '1.2px' }} title="Sync to metrc again" style={{ color: 'orange' }} />
-                    </div> */}
-
-                <List {...this.props} actions={<ListActionButton disable={this.state.disable} lastSynched = {this.state.lastSynched}  syncClick={this.syncClick} />} >
+                <List
+                    {...this.props}
+                    key={this.state.key}
+                    actions={<ListActionButton disable={this.state.disable}
+                    lastSynched={this.state.lastSynched}
+                    syncClick={this.syncClick} />}
+                >
                     <Datagrid >
                         {/* <ReferenceField source="uId" reference="us"><TextField source="id" /></ReferenceField> */}
                         <TextField label="Manifest Number" source="manifestNumber" />
@@ -77,7 +77,7 @@ constructor(props){
 
                     </Datagrid>
                 </List>
-              </div>
+            </div>
 
         );
 
